bc4ff56ec00216c0ffee11dbf42514e48b719db8
Sc23aj2 <146622139+Sc23aj2@users.noreply.github.com>
2023-10-29 20:32:12 +0000

    Doubt cleared

--- C_Bootcamp_4/array.c
+++ C_Bootcamp_4/array.c
@@ -2,9 +2,13 @@
 
 int main ()
 {
-    int array;
-    array[5] = {1, 2, 3, 4, 5};
+    int array[5] = {1, 2, 3, 4, 5};
 
     int i;
-    for (i=)
+    int array_n[5] = {1, 1, 1, 1, 1};
+
+    for (i=0; i < 5; i += 1){
+        array_n[i] = array[4 -i];
+        printf("%d\n", array_n[i]);
+    }
 }
