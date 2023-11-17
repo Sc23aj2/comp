b48a690e49dbf72b56805127c8fcede4660fa03f
Sc23aj2 <146622139+Sc23aj2@users.noreply.github.com>
2023-11-17 18:03:46 +0000

    Week7 Session2

---
+++ Coursework1/FitnessDataStruct.h
@@ -0,0 +1,28 @@
+#ifndef FITNESS_DATA_STRUCT_H
+#define FITNESS_DATA_STRUCT_H
+
+// Define an appropriate struct
+#include <stdio.h>
+
+typedef struct {
+	char date[11];
+	char time[6];
+	int steps;
+} FITNESS_DATA;
+
+// Helper function prototypes
+void tokeniseRecord(const char *input, const char *delimiter, char *date, char *time, char *steps);
+
+FILE *open_file(char *filename, char *mode){
+    FILE *file = fopen(filename, mode);
+    if (file == NULL){
+        printf("Error: Could not find or open the file.\n");
+        exit(1);
+    }
+    else{
+    printf("File successfully loaded.\n");    
+    return file;
+    }
+}
+
+#endif // FITNESS_DATA_STRUCT_H
---
+++ Coursework1/StepCounter_Final.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "FitnessDataStruct.h"
+
+// Struct moved to header file
+
+// Define any additional variables here
+// Global variables for filename and FITNESS_DATA array
+    char choice;
+    int buffer_size = 100;
+    FITNESS_DATA fitness_struct[100];
+    
+   
+
+
+// This is your helper function. Do not change it in any way.
+// Inputs: character array representing a row; the delimiter character
+// Ouputs: date character array; time character array; steps character array
+void tokeniseRecord(const char *input, const char *delimiter,
+                    char *date, char *time, char *steps) {
+    // Create a copy of the input string as strtok modifies the string
+    char *inputCopy = strdup(input);
+    
+    // Tokenize the copied string
+    char *token = strtok(inputCopy, delimiter);
+    if (token != NULL) {        strcpy(date, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(time, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(steps, token);
+    }
+    
+    // Free the duplicated string
+    free(inputCopy);
+
+}
+
+// Complete the main function
+int main() {
+    printf("Menu Options:\n");
+    printf("A: Specify the filename to be imported\n");  
+    printf("B: Display the total number of records in the file\n");
+    printf("C: Find the date and time of the timeslot with the fewest steps\n");
+    printf("D: Find the date and time of the timeslot with the largest number of steps\n");
+    printf("E: Find the mean step count of all the records in the file\n");
+    printf("F: Find the longest continuous period where the step count is above 500 steps\n");
+    printf("Q: Quit\n");
+
+    choice = getchar();
+
+    while (getchar() != '\n');
+
+        char line[buffer_size];
+        char fitness[buffer_size];
+
+        char date[11];
+        char time[6];
+        char steps[10];
+        int i = 0;
+        char line_buffer_2[buffer_size];
+
+    switch(choice){
+
+    case 'A':
+    case 'a':
+        printf("Input filename: \n");
+
+        fgets(line, buffer_size, stdin);
+        sscanf(line, "%s", fitness);
+
+        FILE *fitness_data = open_file(fitness, "r");
+        
+        break;
+
+    case 'B':
+    case 'b':
+    while (fgets(line_buffer_2, buffer_size, fitness_data)) {
+        tokeniseRecord(line_buffer_2, ",", date, time, steps);
+        strcpy(fitness_struct[i].date, date);
+        strcpy(fitness_struct[i].time, time);
+        fitness_struct[i].steps = atoi(steps);
+
+        i += 1;
+    }
+        printf("Number of records in file: %d\n", i);
+
+        break;
+    }    
+
+
+
+
+   
+   
+  
+
+   
+   
+    // int a;
+
+    // for (a = 0; a < 3; a += 1){
+    //     printf("%s/%s/%d\n",  fitness_struct[a].date, fitness_struct[a].time, fitness_struct[a].steps);
+    // }
+}
